854b3953229890fae59dc7103c69cd186d51cfc4
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-04-22 09:38:43 +0200

    04.22

---
+++ C#/fuvar/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace fuvar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            betolt();
+        }
+
+        List<Fuvar> fuvarok = new List<Fuvar>();
+
+        void betolt()
+        {
+            string[] sorok = File.ReadAllLines("fuvar.csv");
+
+            foreach (var item in sorok.Skip(1))
+            {
+                fuvarok.Add(new Fuvar(item));
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void f3_Click(object sender, RoutedEventArgs e)
+        {
+            f3Eredmeny.Content = fuvarok.Count;
+        }
+    }
+}
