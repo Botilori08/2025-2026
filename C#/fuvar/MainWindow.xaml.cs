63d27131f5c110d7cac8172edd30c6b2f4973983
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-04-22 11:25:57 +0200

    04.22

--- C#/fuvar/MainWindow.xaml.cs
+++ C#/fuvar/MainWindow.xaml.cs
@@ -35,6 +35,9 @@ namespace fuvar
                 fuvarok.Add(new Fuvar(item));
             }
 
+            f4Lista.ItemsSource = fuvarok.Select(e => e.azonosito).Distinct().OrderBy(e => e).ToList();
+
+
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -46,5 +49,42 @@ namespace fuvar
         {
             f3Eredmeny.Content = fuvarok.Count;
         }
+
+        private void f4Lista_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox valaszt = sender as ComboBox;
+
+            int kivalasztott = Convert.ToInt32(valaszt.SelectedItem);
+
+            
+            var bevetel = fuvarok.Where(e => e.azonosito == kivalasztott).Select(e => e.viteldij + e.borravalo).Sum(e => e);
+
+            var fuvarSzam = fuvarok.Where(e => e.azonosito == kivalasztott).Count();
+
+            //MessageBox.Show($"{bevetel}      {fuvarSzam}");
+
+            Bevetel.Content = bevetel;
+            FuvarSzam.Content = fuvarSzam;
+            
+
+            /*
+             * Másik megoldás
+             * double penz = 0;
+             * int fuvarszam = 0
+
+            foreach(Fuvar egyFuvar in fuvarok)
+            {
+                if(egyFuvar.azonosito == kivalasztott)
+                {
+                    penz = egyFuvar.viteldij + egyFuvar.borravalo;
+                    fuvarszam++;
+                }
+            }
+            //MessageBox.Show(penz.ToString());
+            */
+        }
+
+
+
     }
 }
