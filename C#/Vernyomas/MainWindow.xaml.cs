f406bd9bd37fbed0f11c4eb4a7b29593bb9995a2
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-04-27 13:17:12 +0200

    04.27

---
+++ C#/Vernyomas/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vernyomas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            listaKeszit();
+        }
+
+        void listaKeszit()
+        {
+            List<string> Napszakok = new List<string>() { "reggel", "délben", "este" };
+
+            napszakValaszt.ItemsSource = Napszakok;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int napSzama;
+
+            bool mindJo = false;
+
+            try
+            {
+                napSzama = Convert.ToInt32(napSzam.Text);
+                if (napSzama > 0 && napSzama < 31)
+                {
+                    napSzama = Convert.ToInt32(napSzam.Text);
+
+                }
+                else {
+                    napSzam.Text = "";
+                    
+
+                }
+            }
+            catch (Exception ex)
+            {
+                napSzam.Text = "";
+                
+            }
+
+
+            try
+            {
+                napSzama = Convert.ToInt32(napSzam.Text);
+                if (napSzama > 0 && napSzama < 31)
+                {
+                    napSzama = Convert.ToInt32(napSzam.Text);
+                }
+                else
+                {
+                    napSzam.Text = "";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                napSzam.Text = "";
+            }
+
+        }
+    }
+}
