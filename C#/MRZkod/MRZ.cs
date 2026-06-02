74daffa1e4e2ca59c9803e2ce8526e6d85a1439f
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-06-02 09:53:43 +0200

    06.02

---
+++ C#/MRZkod/MRZ.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MRZkod
+{
+	internal class MRZ
+	{
+		public string tipus;
+		public string orszag;
+		public string csaladiEsutonev;
+		public string okmanySzam;
+
+		public string okmanyszamEll;
+		public string nemzetiseg;
+		public string szuletesiIdo;
+		public string szuletesiidoEll;
+		public string nem;
+		public string ervenyesseg;
+		public string ervenyessegEllenorzo;
+		public string okmanytkiadovalaszthatoadat;
+		public string okmanyValaszthatoEll;
+		public string osszesitettEll;
+
+		public MRZ(string elsoSor,string masodikSor)
+		{
+			string[] sorok = elsoSor.Split("");
+			tipus = elsoSor.Substring(0, 2);
+			orszag = elsoSor.Substring(2, 3);
+			csaladiEsutonev = elsoSor.Substring(5, 39);
+
+			//012345678
+			okmanySzam = masodikSor.Substring(0, 9);
+			okmanyszamEll = masodikSor.Substring(9, 1);
+			nemzetiseg = masodikSor.Substring(10, 3);
+			szuletesiIdo = masodikSor.Substring(13,6);
+			szuletesiidoEll = masodikSor.Substring(19, 1);
+			nem = masodikSor.Substring(20, 1);
+			ervenyesseg = masodikSor.Substring(21, 6);
+			ervenyessegEllenorzo = masodikSor.Substring(27, 1);
+			okmanytkiadovalaszthatoadat = masodikSor.Substring(28, 14);
+			okmanyValaszthatoEll = masodikSor.Substring(42, 1);
+			osszesitettEll = masodikSor.Substring(43,1);
+		}
+
+
+		public bool csonkolt()
+		{
+			return this.csaladiEsutonev[this.csaladiEsutonev.Length - 1] != '<';
+		}
+
+
+	}
+}
---
+++ C#/MRZkod/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace MRZkod
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("1. feladat");
+
+			Console.Write("Az állomány neve: ");
+
+			string fileNev = Console.ReadLine();
+
+			string[] sorok = File.ReadAllLines(fileNev);
+
+
+			Console.WriteLine(string.Join("\n", sorok));
+
+			//string MRZKod = string.Join("", sorok);
+
+			//Console.WriteLine(MRZKod);
+
+			//Console.WriteLine(MRZKod.Substring(2,3));
+
+			MRZ mrz = new MRZ(sorok[0],sorok[1]);
+
+			Console.WriteLine("2. feladat");
+			if(mrz.nem == "F")
+			{
+				Console.WriteLine("Az okmány tulajdonosa nő.");
+			}
+			else
+			{
+				Console.WriteLine("Az okmány tulajdonosa férfi.");
+			}
+
+			Console.WriteLine("3.feladat");
+
+			Console.Write("Az aktuális dátum:");
+			string datum = Console.ReadLine();
+
+			string mrzDatum = mrz.ervenyesseg;
+
+			if(Convert.ToInt32(mrzDatum.Substring(0,2)) <= Convert.ToInt32(datum.Substring(0,2)) || Convert.ToInt32(mrzDatum.Substring(3, 2)) <= Convert.ToInt32(datum.Substring(3, 2)) || Convert.ToInt32(mrzDatum.Substring(5, 2)) <= Convert.ToInt32(datum.Substring(5, 2)))
+			{
+                Console.WriteLine("Érvényes.");
+            }
+			else
+			{
+				Console.WriteLine("Lejárt.");
+			}
+
+            Console.WriteLine("4.feladat");
+
+			string[] neve = mrz.csaladiEsutonev.Split("<");
+
+			List<string> nev = neve.Where(x => x != "").ToList();
+
+			/*foreach (string s in nev)
+			{
+                Console.WriteLine(s);
+			}*/
+
+			if (nev.Count == 4)
+			{
+                Console.WriteLine($"Családi név: {nev[0]} {nev[1]}");
+                Console.WriteLine($"Utónév: {nev[2]} {nev[3]}");
+            }
+			else
+			{
+                Console.WriteLine($"Családi név: {nev[0]} {nev[1]}");
+                Console.WriteLine($"Utónév: {nev[2]}");
+            }
+
+			if(mrz.csonkolt())
+			{
+                Console.WriteLine("Lehetséges, hogy csonkolt a név.");
+            }
+			else
+			{
+                Console.WriteLine("A név nem csonkolt.");
+			}
+
+
+			
+		}
+	}
+}
