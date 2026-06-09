13179688507140352ab4bc5c43bc700c92cb2319
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-06-09 14:55:52 +0200

    06.09

--- C#/palacsinta/MainWindow.xaml.cs
+++ C#/palacsinta/MainWindow.xaml.cs
@@ -12,70 +12,72 @@ using System.IO;
 
 namespace palacsinta
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            betolt();
-            
-        }
+			betolt();
+			
+		}
 
-        List<int> arak = new List<int>();
-        void betolt()
-        {
-            string[] sorok = File.ReadAllLines("arak.txt");
+		List<int> arak = new List<int>();
+		void betolt()
+		{
+			string[] sorok = File.ReadAllLines("arak.txt");
 
-            foreach (var item in sorok[0].Split(", "))
-            {
-                //MessageBox.Show(item);
-                arak.Add(Convert.ToInt32(item));
-            }
-
-
-        }
-
-        private void f1_Click(object sender, RoutedEventArgs e)
-        {
-            int napSzam = Convert.ToInt32(szam.Text);
-
-            f2eredmeny.Content = $"A {napSzam}. napon {arak[napSzam - 1]} Ft volt egy adag palacsinta.";
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var darabszam = arak.Select(e => 4000 / e).ToList();
-
-            f3.ItemsSource = darabszam;
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-            List<string> kiiras = new List<string>();
+			foreach (var item in sorok[0].Split(", "))
+			{
+				//MessageBox.Show(item);
+				arak.Add(Convert.ToInt32(item));
+			}
 
 
-            int kolthetoOsszeg = 0;
-            for (int i = 0; i < arak.Count; i++)
-            {
+		}
 
-                kolthetoOsszeg += 4000;
-                int palacsintaSzam = kolthetoOsszeg / arak[i];
+		private void f1_Click(object sender, RoutedEventArgs e)
+		{
+			int napSzam = Convert.ToInt32(szam.Text);
 
-                kolthetoOsszeg -= palacsintaSzam * arak[i];
+			f2eredmeny.Content = $"A {napSzam}. napon {arak[napSzam - 1]} Ft volt egy adag palacsinta.";
+		}
 
-                kiiras.Add($"A(z) {i + 1}. napon {palacsintaSzam} adag palacsintát vettek.");
-                 
-            }
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var darabszam = arak.Select(e => 4000 / e).ToList();
 
-            f4.ItemsSource = kiiras;
+			f3.ItemsSource = darabszam;
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+
+			
+
+			List<string> kiiras = new List<string>();
+
+
+			int kolthetoOsszeg = 0;
+			for (int i = 0; i < arak.Count; i++)
+			{
+
+				kolthetoOsszeg += 4000;
+				int palacsintaSzam = kolthetoOsszeg / arak[i];
+
+				kolthetoOsszeg -= palacsintaSzam * arak[i];
+
+				kiiras.Add($"A(z) {i + 1}. napon {palacsintaSzam} adag palacsintát vettek.");
+				 
+			}
+
+			f4.ItemsSource = kiiras;
 
 
 
-        }
-    }
+		}
+	}
 }
